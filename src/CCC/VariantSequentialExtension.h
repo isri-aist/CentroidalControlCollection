cf079afa6c28e968edcb8ea6e77070dd130db37d
Masaki Murooka <m-murooka@aist.go.jp>
2022-02-24 22:44:12 +0900

    support extend_for_output.

--- src/CCC/VariantSequentialExtension.h
+++ src/CCC/VariantSequentialExtension.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include <iostream>
 #include <memory>
 
 #include <CCC/StateSpaceModel.h>
@@ -24,29 +25,69 @@ public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
   /** \brief Constructor.
-     \param model_list list of state-space model
+      \param model_list list of state-space model
+      \param extend_for_output whether to extend for output instead of state
    */
-  VariantSequentialExtension(const ListType<std::shared_ptr<_StateSpaceModel>> & model_list)
+  VariantSequentialExtension(const ListType<std::shared_ptr<_StateSpaceModel>> & model_list,
+                             bool extend_for_output = false)
   : model_list_(model_list)
   {
-    setup();
+    setup(extend_for_output);
+  }
+
+  /** \brief Get total state dimension. */
+  int totalStateDim() const
+  {
+    return total_state_dim_;
+  }
+
+  /** \brief Get total input dimension. */
+  int totalInputDim() const
+  {
+    return total_input_dim_;
+  }
+
+  /** \brief Get total output dimension. */
+  int totalOutputDim() const
+  {
+    return total_output_dim_;
   }
 
 protected:
   /** \brief Setup coefficients. */
-  void setup()
+  void setup(bool extend_for_output)
   {
+    // Check D matrix
+    if(extend_for_output)
+    {
+      for(const auto & model : model_list_)
+      {
+        if(model->D_.norm() > 0)
+        {
+          std::cerr
+              << "[VariantSequentialExtension] Matrix D in state-space model must be zero when extending for output.\n"
+              << "  Matrix D:\n"
+              << model->D_;
+          break;
+        }
+      }
+    }
+
+    // Set total dimension
     size_t seq_len = model_list_.size();
-    int total_input_dim = 0;
+    total_state_dim_ = seq_len * StateDim;
+    total_input_dim_ = 0;
+    total_output_dim_ = 0;
     for(const auto & model : model_list_)
     {
-      total_input_dim += model->inputDim();
+      total_input_dim_ += model->inputDim();
+      total_output_dim_ += model->outputDim();
     }
 
     // Resize matrix
-    A_seq_.setZero(seq_len * StateDim, StateDim);
-    B_seq_.setZero(seq_len * StateDim, total_input_dim);
-    E_seq_.setZero(seq_len * StateDim);
+    A_seq_.setZero(total_state_dim_, StateDim);
+    B_seq_.setZero(total_state_dim_, total_input_dim_);
+    E_seq_.setZero(total_state_dim_);
 
     int accum_input_dim = 0;
     for(size_t i = 0; i < seq_len; i++)
@@ -91,11 +132,41 @@ protected:
 
       accum_input_dim += current_input_dim;
     }
+
+    // Transform for output
+    if(extend_for_output)
+    {
+      // Set C_seq
+      Eigen::MatrixXd C_seq = Eigen::MatrixXd::Zero(total_output_dim_, total_state_dim_);
+      int accum_output_dim = 0;
+      for(size_t i = 0; i < seq_len; i++)
+      {
+        int current_output_dim = model_list_[i]->outputDim();
+
+        C_seq.template block(accum_output_dim, i * StateDim, current_output_dim, StateDim) = model_list_[i]->C_;
+
+        accum_output_dim += current_output_dim;
+      }
+
+      // Apply C_seq
+      A_seq_ = C_seq * A_seq_;
+      B_seq_ = C_seq * B_seq_;
+      E_seq_ = C_seq * E_seq_;
+    }
   }
 
 public:
   //! State-space model list
   ListType<std::shared_ptr<_StateSpaceModel>> model_list_;
+
+  //! Total state dimension
+  int total_state_dim_ = 0;
+
+  //! Total input dimension
+  int total_input_dim_ = 0;
+
+  //! Total output dimension
+  int total_output_dim_ = 0;
 
   //! Sequential extension matrix of A in discrete system
   Eigen::Matrix<double, Eigen::Dynamic, StateDim> A_seq_;
--- tests/src/TestVariantSequentialExtension.cpp
+++ tests/src/TestVariantSequentialExtension.cpp
@@ -8,17 +8,20 @@
 #include <CCC/EigenTypes.h>
 #include <CCC/VariantSequentialExtension.h>
 
-class Model1 : public CCC::StateSpaceModel<3, Eigen::Dynamic, Eigen::Dynamic>
+constexpr int state_dim = 3;
+
+class Model1 : public CCC::StateSpaceModel<state_dim, Eigen::Dynamic, Eigen::Dynamic>
 {
 public:
-  Model1() : StateSpaceModel(3, 1, 1)
+  Model1() : StateSpaceModel(state_dim, 1, 1)
   {
     A_(0, 1) = 1;
     A_(1, 2) = 1;
 
     B_(2) = 1;
 
-    C_(0) = 1;
+    C_(0, 0) = 1;
+    C_(0, 2) = 2;
   }
 };
 
@@ -27,43 +30,116 @@ class Model2 : public Model1
 public:
   Model2()
   {
-    D_ << 5.0;
-
     E_ << -1.0, 2.0, -3.0;
   }
 };
 
-class Model3 : public CCC::StateSpaceModel<3, Eigen::Dynamic, Eigen::Dynamic>
+class Model3 : public CCC::StateSpaceModel<state_dim, Eigen::Dynamic, Eigen::Dynamic>
 {
 public:
-  Model3(int idx) : StateSpaceModel(3, 0, 1)
+  Model3(int idx) : StateSpaceModel(state_dim, 0, 1)
   {
     A_(0, 1) = 0.1 * idx;
     A_(1, 2) = 0.2 * idx * idx;
 
-    C_(0) = 1;
+    C_(0, 0) = 1;
+    C_(0, 1) = -1;
 
     E_ << -0.1 * idx, 0.2 * idx * idx, -0.3;
   }
 };
 
-template<class ModelType>
-void testVariantSequentialExtension(bool debug = false)
+template<template<class> class ListType>
+Eigen::VectorXd calcStateSeq(const CCC::VariantSequentialExtension<state_dim, ListType> & seq_ext,
+                             const Eigen::VectorXd & u_seq,
+                             const Eigen::Matrix<double, state_dim, 1> & x_initial,
+                             bool extend_for_output)
 {
-  constexpr int state_dim = 3;
-  size_t seq_len = 5;
-
-  // Setup model
-  std::vector<std::shared_ptr<CCC::StateSpaceModel<state_dim, Eigen::Dynamic, Eigen::Dynamic>>> model_list;
-  model_list.assign(seq_len, std::make_shared<ModelType>());
-  double dt = 1e-2;
-  for(const auto & model : model_list)
+  Eigen::VectorXd x_seq_iter(extend_for_output ? seq_ext.totalOutputDim() : seq_ext.totalStateDim());
+  size_t accum_input_dim = 0;
+  size_t accum_output_dim = 0;
+  Eigen::Matrix<double, state_dim, 1> current_x = x_initial;
+  for(size_t i = 0; i < seq_ext.model_list_.size(); i++)
   {
-    model->calcDiscMatrix(dt);
+    int current_input_dim = seq_ext.model_list_[i]->inputDim();
+    int current_output_dim = seq_ext.model_list_[i]->outputDim();
+    current_x = seq_ext.model_list_[i]->stateEqDisc(current_x, u_seq.segment(accum_input_dim, current_input_dim));
+    if(extend_for_output)
+    {
+      x_seq_iter.template segment(accum_output_dim, current_output_dim) = seq_ext.model_list_[i]->observEq(current_x);
+    }
+    else
+    {
+      x_seq_iter.template segment<state_dim>(i * state_dim) = current_x;
+    }
+    accum_input_dim += current_input_dim;
+    accum_output_dim += current_output_dim;
   }
+  return x_seq_iter;
+}
+
+template<template<class> class ListType>
+void printStateSeq(const CCC::VariantSequentialExtension<state_dim, ListType> & seq_ext,
+                   Eigen::VectorXd x_seq_ext, // Pass by value for Eigen::Map
+                   Eigen::VectorXd x_seq_iter,
+                   bool extend_for_output)
+{
+  if(extend_for_output)
+  {
+    {
+      std::cout << "x_seq_ext:\n";
+      int accum_output_dim = 0;
+      for(size_t i = 0; i < seq_ext.model_list_.size(); i++)
+      {
+        int current_output_dim = seq_ext.model_list_[i]->outputDim();
+        std::cout << x_seq_ext.segment(accum_output_dim, current_output_dim) << std::endl;
+        accum_output_dim += current_output_dim;
+      }
+    }
+    {
+      std::cout << "x_seq_iter:\n";
+      int accum_output_dim = 0;
+      for(size_t i = 0; i < seq_ext.model_list_.size(); i++)
+      {
+        int current_output_dim = seq_ext.model_list_[i]->outputDim();
+        std::cout << x_seq_iter.segment(accum_output_dim, current_output_dim) << std::endl;
+        accum_output_dim += current_output_dim;
+      }
+    }
+    {
+      std::cout << "error:\n";
+      int accum_output_dim = 0;
+      for(size_t i = 0; i < seq_ext.model_list_.size(); i++)
+      {
+        int current_output_dim = seq_ext.model_list_[i]->outputDim();
+        std::cout << (x_seq_ext - x_seq_iter).segment(accum_output_dim, current_output_dim) << std::endl;
+        accum_output_dim += current_output_dim;
+      }
+    }
+  }
+  else
+  {
+    Eigen::Map<Eigen::MatrixXd> x_seq_ext_reshaped(x_seq_ext.data(), state_dim, seq_ext.model_list_.size());
+    Eigen::Map<Eigen::MatrixXd> x_seq_iter_reshaped(x_seq_iter.data(), state_dim, seq_ext.model_list_.size());
+    std::cout << "x_seq_ext:\n" << x_seq_ext_reshaped.transpose() << std::endl;
+    std::cout << "x_seq_iter:\n" << x_seq_iter_reshaped.transpose() << std::endl;
+    std::cout << "error:\n" << (x_seq_ext_reshaped - x_seq_iter_reshaped).transpose() << std::endl;
+  }
+}
+
+template<class ModelType>
+void testVariantSequentialExtension(bool extend_for_output, bool debug = false)
+{
+  // Setup model
+  size_t seq_len = 5;
+  double dt = 1e-2;
+  const auto & model = std::make_shared<ModelType>();
+  model->calcDiscMatrix(dt);
+  std::vector<std::shared_ptr<CCC::StateSpaceModel<state_dim, Eigen::Dynamic, Eigen::Dynamic>>> model_list(seq_len,
+                                                                                                           model);
 
   // Calculate sequential extension
-  CCC::VariantSequentialExtension<state_dim> seq_ext(model_list);
+  CCC::VariantSequentialExtension<state_dim> seq_ext(model_list, extend_for_output);
   if(debug)
   {
     std::cout << "A_seq:\n" << seq_ext.A_seq_ << std::endl;
@@ -71,79 +147,59 @@ void testVariantSequentialExtension(bool debug = false)
     std::cout << "E_seq:\n" << seq_ext.E_seq_ << std::endl;
   }
 
-  Eigen::Vector3d x_initial;
+  // Calculate state sequence
+  Eigen::Matrix<double, state_dim, 1> x_initial;
   x_initial << 1, 2, 3;
   Eigen::VectorXd u_seq(seq_len);
   u_seq << 5.0, 2.5, 0.0, -1.0, -2.0;
 
   Eigen::VectorXd x_seq_ext = seq_ext.A_seq_ * x_initial + seq_ext.B_seq_ * u_seq + seq_ext.E_seq_;
+  Eigen::VectorXd x_seq_iter = calcStateSeq(seq_ext, u_seq, x_initial, extend_for_output);
 
   // Compare results from extension and iteration
-  Eigen::VectorXd x_seq_iter(state_dim * seq_len);
-  for(size_t i = 0; i < seq_len; i++)
-  {
-    if(i == 0)
-    {
-      x_seq_iter.template segment<state_dim>(i * state_dim) =
-          model_list[i]->stateEqDisc(x_initial, u_seq.segment(i, 1));
-    }
-    else
-    {
-      x_seq_iter.template segment<state_dim>(i * state_dim) =
-          model_list[i]->stateEqDisc(x_seq_iter.template segment<state_dim>((i - 1) * state_dim), u_seq.segment(i, 1));
-    }
-  }
-
+  EXPECT_TRUE((x_seq_ext - x_seq_iter).norm() < 1e-10);
   if(debug)
   {
-    Eigen::Map<Eigen::MatrixXd> x_seq_ext_reshaped(x_seq_ext.data(), state_dim, seq_len);
-    Eigen::Map<Eigen::MatrixXd> x_seq_iter_reshaped(x_seq_iter.data(), state_dim, seq_len);
-    std::cout << "x_seq_ext:\n" << x_seq_ext_reshaped << std::endl;
-    std::cout << "x_seq_iter:\n" << x_seq_iter_reshaped << std::endl;
-    std::cout << "error:\n" << x_seq_ext_reshaped - x_seq_iter_reshaped << std::endl;
+    printStateSeq(seq_ext, x_seq_ext, x_seq_iter, extend_for_output);
   }
-
-  EXPECT_TRUE((x_seq_ext - x_seq_iter).norm() < 1e-10);
 }
 
 TEST(TestVariantSequentialExtension, Model1)
 {
-  testVariantSequentialExtension<Model1>();
+  testVariantSequentialExtension<Model1>(false);
+  testVariantSequentialExtension<Model1>(true);
 }
 
 TEST(TestVariantSequentialExtension, Model2)
 {
-  testVariantSequentialExtension<Model2>();
+  testVariantSequentialExtension<Model2>(false);
+  testVariantSequentialExtension<Model2>(true);
 }
 
-TEST(TestVariantSequentialExtension, VariantInput)
+void testVariantSequentialExtensionVariantInput(bool extend_for_output, bool debug = false)
 {
-  bool debug = false;
-  constexpr int state_dim = 3;
-  size_t seq_len = 10;
-  std::set<size_t> no_input_steps = {4, 5, 6, 8};
-
   // Setup model
+  size_t seq_len = 10;
+  double dt = 1e-2;
+  std::set<size_t> no_input_steps = {4, 5, 6, 8};
   std::deque<std::shared_ptr<CCC::StateSpaceModel<state_dim, Eigen::Dynamic, Eigen::Dynamic>>> model_list;
   for(size_t i = 0; i < seq_len; i++)
   {
+    std::shared_ptr<CCC::StateSpaceModel<state_dim, Eigen::Dynamic, Eigen::Dynamic>> model;
     if(no_input_steps.count(i))
     {
-      model_list.push_back(std::make_shared<Model3>(i));
+      model = std::make_shared<Model3>(i);
     }
     else
     {
-      model_list.push_back(std::make_shared<Model1>());
+      model = std::make_shared<Model1>();
     }
-  }
-  double dt = 1e-2;
-  for(const auto & model : model_list)
-  {
     model->calcDiscMatrix(dt);
+    model_list.push_back(model);
   }
 
   // Calculate sequential extension
-  CCC::VariantSequentialExtension<state_dim, std::deque> seq_ext(model_list);
+  CCC::VariantSequentialExtension<state_dim, std::deque> seq_ext(model_list, extend_for_output);
   if(debug)
   {
     std::cout << "A_seq:\n" << seq_ext.A_seq_ << std::endl;
@@ -151,43 +207,27 @@ TEST(TestVariantSequentialExtension, VariantInput)
     std::cout << "E_seq:\n" << seq_ext.E_seq_ << std::endl;
   }
 
-  Eigen::Vector3d x_initial;
+  // Calculate state sequence
+  Eigen::Matrix<double, state_dim, 1> x_initial;
   x_initial << 1, 2, 3;
   Eigen::VectorXd u_seq(seq_len - no_input_steps.size());
   u_seq << 0.5, 1.0, 0.5, 1.0, -1.0, -2.0;
 
   Eigen::VectorXd x_seq_ext = seq_ext.A_seq_ * x_initial + seq_ext.B_seq_ * u_seq + seq_ext.E_seq_;
+  Eigen::VectorXd x_seq_iter = calcStateSeq(seq_ext, u_seq, x_initial, extend_for_output);
 
   // Compare results from extension and iteration
-  Eigen::VectorXd x_seq_iter(state_dim * seq_len);
-  size_t accum_input_dim = 0;
-  for(size_t i = 0; i < seq_len; i++)
-  {
-    int current_input_dim = (no_input_steps.count(i) ? 0 : 1);
-    if(i == 0)
-    {
-      x_seq_iter.template segment<state_dim>(i * state_dim) =
-          model_list[i]->stateEqDisc(x_initial, u_seq.segment(accum_input_dim, current_input_dim));
-    }
-    else
-    {
-      x_seq_iter.template segment<state_dim>(i * state_dim) =
-          model_list[i]->stateEqDisc(x_seq_iter.template segment<state_dim>((i - 1) * state_dim),
-                                     u_seq.segment(accum_input_dim, current_input_dim));
-    }
-    accum_input_dim += current_input_dim;
-  }
-
+  EXPECT_TRUE((x_seq_ext - x_seq_iter).norm() < 1e-10);
   if(debug)
   {
-    Eigen::Map<Eigen::MatrixXd> x_seq_ext_reshaped(x_seq_ext.data(), state_dim, seq_len);
-    Eigen::Map<Eigen::MatrixXd> x_seq_iter_reshaped(x_seq_iter.data(), state_dim, seq_len);
-    std::cout << "x_seq_ext:\n" << x_seq_ext_reshaped << std::endl;
-    std::cout << "x_seq_iter:\n" << x_seq_iter_reshaped << std::endl;
-    std::cout << "error:\n" << x_seq_ext_reshaped - x_seq_iter_reshaped << std::endl;
+    printStateSeq(seq_ext, x_seq_ext, x_seq_iter, extend_for_output);
   }
+}
 
-  EXPECT_TRUE((x_seq_ext - x_seq_iter).norm() < 1e-10);
+TEST(TestVariantSequentialExtension, VariantInput)
+{
+  testVariantSequentialExtensionVariantInput(false);
+  testVariantSequentialExtensionVariantInput(true);
 }
 
 int main(int argc, char ** argv)
