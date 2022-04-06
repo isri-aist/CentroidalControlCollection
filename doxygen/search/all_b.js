eb16ce48d48897f7c89f6eaad008672ee7a73884
Masaki Murooka <m-murooka@aist.go.jp>
2022-04-06 13:22:01 +0000

    Update Doxygen HTML files from commit cbeeda458ae698d0310b3f090b04dfa054351d22

--- doxygen/search/all_b.js
+++ doxygen/search/all_b.js
@@ -2,8 +2,8 @@ var searchData=
 [
   ['linear_5fmomentum',['linear_momentum',['../structCCC_1_1LinearMpcXY_1_1WeightParam.html#a6ed673e5c3fa32a313e7ba38ec059140',1,'CCC::LinearMpcXY::WeightParam']]],
   ['linear_5fmomentum_5fintegral',['linear_momentum_integral',['../structCCC_1_1LinearMpcXY_1_1WeightParam.html#a50e46a64f7b628d1801c8b97bcc33ea0',1,'CCC::LinearMpcXY::WeightParam']]],
-  ['linearmpcxy',['LinearMpcXY',['../classCCC_1_1LinearMpcXY.html',1,'CCC::LinearMpcXY'],['../classCCC_1_1LinearMpcXY.html#a1387eeecd9a91ab9b6150e655ec79f35',1,'CCC::LinearMpcXY::LinearMpcXY()']]],
+  ['linearmpcxy',['LinearMpcXY',['../classCCC_1_1LinearMpcXY.html',1,'CCC::LinearMpcXY'],['../classCCC_1_1LinearMpcXY.html#a74a8273a8d741149a267a6f79426ffa3',1,'CCC::LinearMpcXY::LinearMpcXY()']]],
   ['linearmpcxy_2eh',['LinearMpcXY.h',['../LinearMpcXY_8h.html',1,'']]],
-  ['linearmpcz',['LinearMpcZ',['../classCCC_1_1LinearMpcZ.html',1,'CCC::LinearMpcZ'],['../classCCC_1_1LinearMpcZ.html#abefb5baa22e71a2fdc96ee224f184300',1,'CCC::LinearMpcZ::LinearMpcZ()']]],
+  ['linearmpcz',['LinearMpcZ',['../classCCC_1_1LinearMpcZ.html',1,'CCC::LinearMpcZ'],['../classCCC_1_1LinearMpcZ.html#a8c37538c55c9637adbb5792844a22a08',1,'CCC::LinearMpcZ::LinearMpcZ()']]],
   ['linearmpcz_2eh',['LinearMpcZ.h',['../LinearMpcZ_8h.html',1,'']]]
 ];
--- doxygen/search/functions_3.js
+++ doxygen/search/functions_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['linearmpcxy',['LinearMpcXY',['../classCCC_1_1LinearMpcXY.html#a1387eeecd9a91ab9b6150e655ec79f35',1,'CCC::LinearMpcXY']]],
-  ['linearmpcz',['LinearMpcZ',['../classCCC_1_1LinearMpcZ.html#abefb5baa22e71a2fdc96ee224f184300',1,'CCC::LinearMpcZ']]]
+  ['linearmpcxy',['LinearMpcXY',['../classCCC_1_1LinearMpcXY.html#a74a8273a8d741149a267a6f79426ffa3',1,'CCC::LinearMpcXY']]],
+  ['linearmpcz',['LinearMpcZ',['../classCCC_1_1LinearMpcZ.html#a8c37538c55c9637adbb5792844a22a08',1,'CCC::LinearMpcZ']]]
 ];
