85de7ba88f8b024a12be0222dd47ac7c462523b1
Masaki Murooka <m-murooka@aist.go.jp>
2022-04-07 18:30:12 +0900

    minor update of tests.

--- tests/src/TestPreviewControlZmp.cpp
+++ tests/src/TestPreviewControlZmp.cpp
@@ -18,10 +18,12 @@ public:
   */
   ComZmpSimModel(double com_height)
   {
-    A_(0, 1) = 1;
-    A_(1, 0) = CCC::constants::g / com_height;
+    double omega2 = CCC::constants::g / com_height;
 
-    B_(1, 0) = -1 * CCC::constants::g / com_height;
+    A_(0, 1) = 1;
+    A_(1, 0) = omega2;
+
+    B_(1, 0) = -1 * omega2;
   }
 };
 
@@ -34,7 +36,7 @@ TEST(TestPreviewControlZmp, Test1)
   // Setup preview control
   CCC::PreviewControlZmp pc(com_height, horizon_duration, horizon_dt);
 
-  // Setup simulation model
+  // Setup simulation
   ComZmpSimModel sim_model(com_height);
   sim_model.calcDiscMatrix(horizon_dt);
 
@@ -78,32 +80,32 @@ TEST(TestPreviewControlZmp, Test1)
 
   std::string file_path = "/tmp/TestPreviewControlZmp.txt";
   std::ofstream ofs(file_path);
-  ofs << "time com_pos com_vel com_acc planned_zmp ref_zmp" << std::endl;
+  ofs << "time com_pos com_vel planned_zmp ref_zmp" << std::endl;
 
   constexpr double end_time = 10.0; // [sec]
   double t = 0;
   while(t < end_time)
   {
-    // Plan ZMP
+    // Plan
     CCC::PreviewControlZmp::InitialParam initial_param;
     initial_param << com_pos_vel, CCC::constants::g / com_height * (com_pos_vel[0] - planned_zmp);
     planned_zmp = pc.planOnce(ref_zmp_func, initial_param, t);
 
     // Dump
     double ref_zmp = ref_zmp_func(t);
-    ofs << t << " " << initial_param.transpose() << " " << planned_zmp << " " << ref_zmp << std::endl;
+    ofs << t << " " << com_pos_vel.transpose() << " " << planned_zmp << " " << ref_zmp << std::endl;
 
-    // Check ZMP
+    // Check
     EXPECT_LT(std::abs(planned_zmp - ref_zmp), 0.1); // [m]
 
-    // Update time and state
+    // Update
     t += horizon_dt;
     Eigen::Vector1d sim_input;
     sim_input << planned_zmp;
     com_pos_vel = sim_model.stateEqDisc(com_pos_vel, sim_input);
   }
 
-  // Check final CoM
+  // Final check
   double ref_zmp = ref_zmp_func(t);
   EXPECT_LT(std::abs(planned_zmp - ref_zmp), 1e-2);
   EXPECT_LT(std::abs(com_pos_vel[0] - ref_zmp), 1e-2);
@@ -112,7 +114,7 @@ TEST(TestPreviewControlZmp, Test1)
   std::cout << "Run the following commands in gnuplot:\n"
             << "  set key autotitle columnhead\n"
             << "  set key noenhanced\n"
-            << "  plot \"" << file_path << "\" u 1:2 w lp, \"\" u 1:5 w lp, \"\" u 1:6 w l lw 2\n";
+            << "  plot \"" << file_path << "\" u 1:2 w lp, \"\" u 1:4 w lp, \"\" u 1:5 w l lw 2\n";
 }
 
 int main(int argc, char ** argv)
