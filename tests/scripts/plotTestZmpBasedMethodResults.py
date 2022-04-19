57a433d843a06034008e0b652d6ae5b672e16f3d
Masaki Murooka <m-murooka@aist.go.jp>
2022-04-19 18:33:35 +0900

    Allow to plot only one method.

--- tests/scripts/plotTestZmpBasedMethodResults.py
+++ tests/scripts/plotTestZmpBasedMethodResults.py
@@ -5,26 +5,36 @@ import matplotlib.pyplot as plt
 
 
 class PlotTestZmpBasedMethodResults(object):
-    def __init__(self, plot_capture_point=False):
+    def __init__(self,
+                 method,
+                 plot_capture_point=False,
+                 plot_comp_time=False):
+        # Setup plot
         self.fig = plt.figure()
         plt.rcParams["font.size"] = 10
-
-        self.result_data_list = {}
+        if isinstance(method, list):
+            method_list = method
+        else:
+            method_list = [method]
+        axis_list = ["x", "y"]
+        subplot_args = [len(method_list), len(axis_list), 0]
+        if plot_comp_time:
+            subplot_args[0] += 1
 
         # Loop for methods
-        self.method_list = ["PreviewControlZmp", "DdpZmp", "DcmTracking", "FootGuidedControl", "LinearMpcZmp"]
-        for method_idx, method_str in enumerate(self.method_list):
+        self.result_data_list = {}
+        for method_idx, method_str in enumerate(method_list):
             # Load file
             result_file_path = "/tmp/Test{}.txt".format(method_str)
+            print("[PlotTestZmpBasedMethodResults] Load {}".format(result_file_path))
             self.result_data_list[method_str] = np.genfromtxt(result_file_path, dtype=None, delimiter=None, names=True)
             result_data = self.result_data_list[method_str]
-            print("[PlotTestZmpBasedMethodResults] Load {}".format(result_file_path))
 
             # Loop for axes
-            self.axis_list = ["x", "y"]
-            for axis_idx, axis_str in enumerate(self.axis_list):
+            for axis_idx, axis_str in enumerate(axis_list):
                 # Setup subplot
-                ax = self.fig.add_subplot(len(self.method_list) + 1, len(self.axis_list), 2 * method_idx + axis_idx + 1)
+                subplot_args[2] = 2 * method_idx + axis_idx + 1
+                ax = self.fig.add_subplot(*subplot_args)
 
                 # Plot
                 planned_zmp_key = "planned_zmp_{}".format(axis_str)
@@ -58,21 +68,41 @@ class PlotTestZmpBasedMethodResults(object):
                     ax.legend(loc="lower right")
 
         # Plot computation time
-        ax = self.fig.add_subplot(len(self.method_list) + 1, len(self.axis_list), len(self.method_list) * len(self.axis_list) + 1)
-        ax.bar(np.arange(len(self.method_list)),
-               [np.mean(self.result_data_list[method_str]["computation_time"]) for method_str in self.method_list],
-               yerr=[np.std(self.result_data_list[method_str]["computation_time"]) for method_str in self.method_list],
-               tick_label=self.method_list, ecolor="black", capsize=5, align="center", log=True)
-        ax.set_title("Computation time")
-        plt.xticks(rotation=20, ha="center")
-        ax.set_ylabel("time [ms]".format(axis_str))
-        ax.grid(axis="y")
+        if plot_comp_time:
+            subplot_args[2] = len(method_list) * len(axis_list) + 1
+            ax = self.fig.add_subplot(*subplot_args)
+            ax.bar(np.arange(len(method_list)),
+                   [np.mean(self.result_data_list[method_str]["computation_time"]) for method_str in method_list],
+                   yerr=[np.std(self.result_data_list[method_str]["computation_time"]) for method_str in method_list],
+                   tick_label=method_list, ecolor="black", capsize=5, align="center", log=True)
+            ax.set_title("Computation time")
+            plt.xticks(rotation=20, ha="center")
+            ax.set_ylabel("time [ms]".format(axis_str))
+            ax.grid(axis="y")
 
         # Show
         # plt.tight_layout()
-        plt.subplots_adjust(left=0.06, bottom=0.06, right=0.98, top=0.96, wspace=0.2, hspace=0.6)
+        if len(method_list) == 1:
+            self.fig.set_size_inches((12, 4))
+            plt.subplots_adjust(left=0.06, bottom=0.14, right=0.98, top=0.90, wspace=0.2, hspace=0.6)
+        else:
+            plt.subplots_adjust(left=0.06, bottom=0.06, right=0.98, top=0.96, wspace=0.2, hspace=0.6)
         plt.show()
 
 
 if __name__ == "__main__":
-    plot = PlotTestZmpBasedMethodResults()
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Plot test results of ZMP-based methods.")
+    all_methods = ["PreviewControlZmp", "DdpZmp", "DcmTracking", "FootGuidedControl", "LinearMpcZmp"]
+    parser.add_argument("--method", "-m", type=str, default="PreviewControlZmp",
+                        choices=all_methods+["All"])
+    parser.add_argument("--plot-capture-point", "-pcp", action="store_true")
+    parser.add_argument("--plot-comp-time", "-pct", action="store_true")
+
+    args = parser.parse_args()
+    if args.method == "All":
+        args.method = all_methods
+    plot = PlotTestZmpBasedMethodResults(method=args.method,
+                                         plot_capture_point=args.plot_capture_point,
+                                         plot_comp_time=args.plot_comp_time)
