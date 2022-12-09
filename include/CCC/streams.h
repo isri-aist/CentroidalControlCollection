903ea8906e8e57ea694146cdc3fcff92abd84b91
Masaki Murooka <m-murooka@aist.go.jp>
2022-12-09 20:01:01 +0900

    Output message into cout for ROS_INFO.
    See http://wiki.ros.org/roscpp/Overview/Logging#Output

--- include/CCC/streams.h
+++ include/CCC/streams.h
@@ -4,7 +4,7 @@
 #  include <iostream>
 #  define CCC_ERROR_STREAM(x) std::cerr << x << "\n"
 #  define CCC_WARN_STREAM(x) std::cerr << x << "\n"
-#  define CCC_INFO_STREAM(x) std::cerr << x << "\n"
+#  define CCC_INFO_STREAM(x) std::cout << x << "\n"
 #else
 #  include <ros/console.h>
 #  define CCC_ERROR_STREAM ROS_ERROR_STREAM
